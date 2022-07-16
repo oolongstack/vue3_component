e4e4e54e6491f7c1a7d90949c0fb7ee6346e279c
dduohereshui <2245675739@qq.com>
2022-07-16 23:00:59 +0800

    feat: bem环境

---
+++ packages/utils/create.ts
@@ -0,0 +1,58 @@
+// bem block块 element元素 modifier装饰
+function _bem(
+  prefixName: string,
+  blockSuffix: string,
+  element: string,
+  modifier: string
+) {
+  if (blockSuffix) {
+    prefixName += `-${blockSuffix}`;
+  }
+  if (element) {
+    prefixName += `__${element}`;
+  }
+  if (modifier) {
+    prefixName += `--${modifier}`;
+  }
+  return prefixName;
+}
+function createBEM(prefixName: string) {
+  const b = (blockSuffix: string = "") => _bem(prefixName, blockSuffix, "", "");
+  const e = (element: string = "") =>
+    element ? _bem(prefixName, "", element, "") : "";
+  const m = (modifier: string = "") =>
+    modifier ? _bem(prefixName, "", "", modifier) : "";
+  const be = (blockSuffix: string = "", element: string = "") =>
+    blockSuffix && element ? _bem(prefixName, element, "", "") : "";
+  const bm = (blockSuffix: string = "", modifier: string = "") =>
+    blockSuffix && modifier ? _bem(prefixName, "", "", modifier) : "";
+  const em = (element: string = "", modifier: string = "") =>
+    element && modifier ? _bem(prefixName, "", element, modifier) : "";
+  const bem = (
+    blockSuffix: string = "",
+    element: string = "",
+    modifier: string = ""
+  ) =>
+    blockSuffix && element && modifier
+      ? _bem(prefixName, blockSuffix, element, modifier)
+      : "";
+  const is = (name: string, state: any) => (state ? `is-${name}` : "");
+  return {
+    b,
+    e,
+    m,
+    be,
+    bm,
+    em,
+    bem,
+    is,
+  };
+}
+export function createNamespace(name: string) {
+  const prefixName = `z-${name}`;
+  return createBEM(prefixName);
+}
+
+// const bem = createNamespace("icon");
+// console.log(bem.b("box"));
+// console.log(bem.e("haha"));
